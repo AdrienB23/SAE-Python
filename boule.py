a88b4567d383f0ec9c29f89714b3e3d825afac27
AdrienB23 <117181235+AdrienB23@users.noreply.github.com>
2022-11-04 19:47:05 +0100

    Ajout des améliorations
    Modifier scinder("lime") et scinder("red") si besoin

--- boule.py
+++ boule.py
@@ -13,9 +13,12 @@ def detection_tour(tour, listeJoueur, numero_tour):
     texte(50, 50,"Tour : " + listeJoueur[tour],"lightcyan", "w",police="",tag="tour")
     texte(1280-50, 50, "Tour n° "+ str(numero_tour) + "/20", "lightcyan","e",police="",tag="tour")
 
-def cerkle(x, y, couleurJoueur, tour, liste_cercle, rayon):
-    tag = cercle(x, y, rayon, couleurJoueur[tour], couleurJoueur[tour])
-    liste_cercle.append([x, y, rayon, tag, tour])
+def cerkle(x, y, couleur, liste_cercle_vert, liste_cercle_violet, rayon, tour):
+    circle = cercle(x, y, rayon, "black", couleur, 1)
+    if couleur == "mediumseagreen":
+        liste_cercle_vert.append([x, y, rayon, circle, tour])
+    else:
+        liste_cercle_violet.append([x, y, rayon, circle, tour])
 
 # def actualiser_score(scores):
 #     efface("tour2")
@@ -30,7 +33,8 @@ def main():
     scores = [0, 0]
     fond()
 
-    liste_cercle = []
+    liste_cercle_violet = []
+    liste_cercle_vert = []
     tour, numero_tour = 0, 1
     texte(0,0,"",tag="tour")
     texte(0,0,"",tag="tour2")
@@ -44,23 +48,55 @@ def main():
         detection_tour(tour, listeJoueur, numero_tour)
 
         #actualiser_score(scores)
-
         if type_ev == "ClicGauche":
-            x, y = clic_x(ev), clic_y(ev)
+            x, y = attente_clic()
+            while x<50 or x>1230 or y<100 or y>670:
+                x, y = attente_clic()
+            
+            if 50<=x<100:
+                x=100
+            elif 1250<x<=1300:
+                x=1250
+            if 100<=y<150:
+                y=150
+            elif 600<y<=650:
+                y=600
+        
             if tour == 1:
                 numero_tour += 1
-
+        
             intersection_cercle = 0
-            intersection_bord = 0
-            for element in liste_cercle:
-                distance = (x-element[0])**2 + (y-element[1])**2
-                if sqrt(distance) < element[2]+rayon and sqrt(distance) > element[2] and element[4] != tour:
-                    intersection_cercle += 1
-                elif sqrt(distance) < element[2] and element[4] != tour:
-                    intersection_cercle += 1
 
-            if intersection_cercle == 0 and 50<=x<=1280-50 and 100<=y<=720-50:
-                cerkle(x, y, couleurJoueur, tour, liste_cercle, rayon)
+        
+#---------Division des cercles rouges---------#
+
+            if listeJoueur[tour] == "Joueur 1":
+                for i in range(len(liste_cercle_violet)):
+                    element = liste_cercle_violet[i]
+                    distance = (x-liste_cercle_violet[i][0])**2 + (y-liste_cercle_violet[i][1])**2
+                    if sqrt(distance) < liste_cercle_violet[i][2] + rayon and sqrt(distance) > liste_cercle_violet[i][2]:
+                        intersection_cercle += 1
+                    elif sqrt(distance) < liste_cercle_violet[i][2]:
+                        scinder("red") #A modifier
+                        intersection_cercle += 1
+                        break
+                    
+#---------Division des cercles verts---------#
+
+            if listeJoueur[tour] == "Joueur 2":
+                for i in range(len(liste_cercle_vert)):
+                    element = liste_cercle_vert[i]
+                    distance = (x-liste_cercle_vert[i][0])**2 + (y-liste_cercle_vert[i][1])**2
+                    if sqrt(distance) < liste_cercle_vert[i][2] + rayon and sqrt(distance) > liste_cercle_vert[i][2]:
+                        intersection_cercle += 1
+                    elif sqrt(distance) < liste_cercle_vert[i][2]:
+                        scinder("lime") # A modifier
+                        intersection_cercle += 1
+                        break
+
+
+            if intersection_cercle == 0:
+                cerkle(x, y, couleurJoueur, tour, liste_cercle_vert, liste_cercle_violet, rayon)
 
                 #scores[tour] += round(pi*(rayon**2))
 
